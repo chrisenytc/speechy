17fb93cf6112ea66e759e87652955dc4dc522381
Christopher EnyTC <chris.enytc@gmail.com>
2015-12-10 09:08:06 -0200

    Add application configs.

---
+++ config/default.js
@@ -0,0 +1,41 @@
+
+module.exports = {
+	temp_dir: process.env.TEMP_DIR || '/tmp/rescue/',
+	speech: {
+		key: process.env.GOOGLE_SPEECH_KEY || '',
+		lang: process.env.GOOGLE_SPEECH_LANGUAGE || 'pt-BR',
+		max_results: process.env.GOOGLE_SPEECH_MAX_RESULTS || '1'
+	},
+	auth: {
+		token: process.env.AUTH_TOKEN || ''
+	},
+	messages: {
+		welcome: {
+			message: 'Welcome to the Speechy Recognition API.'
+		},
+		documentation_url: {
+			message: 'https://github.com/chrisenytc/speechy/#documentation'
+		}
+	},
+	errors: {
+		serverError: {
+			message: 'An error has occurred!'
+		},
+		notFound: {
+			message: 'Page not found.'
+		},
+		required: {
+			message: 'The parameter \'url\' is required!'
+		},
+		invalid: {
+			message: 'Invalid url.'
+		},
+		fail: {
+			message: 'An error has occurred when try to download the audio file!'
+		},
+		unknow: {
+			message: 'Unable to decode the audio!'
+		}
+	}
+}
+
---
+++ config/development.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+	speech: {
+		key: 'ENTER_YOUR_KEY'
+	},
+	auth: {
+		token: 'cii04yvgm0000q48tyfve0agm'
+	}
+};
